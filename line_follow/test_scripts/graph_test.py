a99b22da679e150cd0b752b3038a1ae04cf9db82
Dj <debanikroy92@gmail.com>
2024-01-22 16:48:54 +0900

    graph_tets added

--- line_follow/test_scripts/graph_test.py
+++ line_follow/test_scripts/graph_test.py
@@ -3,22 +3,34 @@ import matplotlib.pyplot as plt
 
 # Define the edges based on the connections you provided
 edges = [
-    (1,2), (2,3), (2,4), 
-    (3,5), (4,7), (5,6), 
-    (5,8), (7,6), (7,9), 
-    (6,10), (10,11), (10,12), 
-    (10,13)
+    (1,2), 
+    (2,3), (2,4),
+    (8,5), (5,3), (5,6), (6,7), (7,4), (7,9),
+    (11, 10), (10, 6), (10, 12), 
+    (10, 13)
 ]
 
-
-# Create an undirected graph
 G = nx.Graph()
-
-# Add edges to the graph
 G.add_edges_from(edges)
 
+# Specify node positions manually
+pos = {
+    1: (0, 0),  # position of node 1
+    2: (1, 0),  # position of node 2
+    3: (2, 1),  # position of node 3
+    4: (2, -1),  # position of node 4
+    8: (3, 2),
+    5: (3, 1),
+    6: (3, 0),
+    7: (3, -1),
+    9: (3, -2),
+    11: (4,1),
+    10: (4,0),
+    12:(4,-1),
+    13: (5, 0)
+}
+
 # Draw the graph
-pos = nx.spring_layout(G)  # You can try different layout algorithms
 nx.draw(G, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=8, font_color="black", font_weight="bold", arrowsize=10)
 
 # Display the graph
