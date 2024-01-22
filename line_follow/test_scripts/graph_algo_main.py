6a5583eb671f9eaeb14e11e25ae08fc8aebc09ff
Dj <debanikroy92@gmail.com>
2024-01-23 08:24:18 +0900

    planner algorithm

--- line_follow/test_scripts/graph_algo_main.py
+++ line_follow/test_scripts/graph_algo_main.py
@@ -2,42 +2,28 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def find_shortest_path(start, end, edges):
-    # Create an undirected graph
     G = nx.Graph()
-
-    # Add edges to the graph
     G.add_edges_from(edges)
-
     try:
-        # Find the shortest path
         shortest_path = nx.shortest_path(G, source=start, target=end)
         return shortest_path
     except nx.NetworkXNoPath:
-        return None  # If there is no path between the nodes
+        return None
 
 def draw_graph(edges, pos, highlight_path=None, start_node=None, end_node=None):
-    # Create a graph
     G = nx.Graph()
     G.add_edges_from(edges)
-
-    # Layout
-    
-
-    # Draw the graph
     nx.draw(G, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=8, font_color="black", font_weight="bold", arrowsize=10)
-
-    # Highlight the path, if specified
+    
     if highlight_path:
         path_edges = list(zip(highlight_path, highlight_path[1:]))
         nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='red', width=2)
-
-    # Mark start and end nodes
+        
     if start_node:
         nx.draw_networkx_nodes(G, pos, nodelist=[start_node], node_color='green', node_size=700)
     if end_node:
         nx.draw_networkx_nodes(G, pos, nodelist=[end_node], node_color='orange', node_size=700)
-
-    # Display the graph
+        
     plt.show()
 
 def junction_analysis(G, path):
@@ -46,14 +32,13 @@ def junction_analysis(G, path):
     for i in range(1, len(path) - 1):
         current_node = path[i]
         neighbors = set(G.neighbors(current_node))
-
-        # Check if the node is a junction
+        
         if len(neighbors) > 2:
             junction_nodes.append(current_node)
 
     return junction_nodes
-    
-# Example usage
+
+
 edges = [
     (1, 2), (2, 3), (2, 4),
     (3, 5), (4, 7), (5, 6),
@@ -65,8 +50,8 @@ edges = [
 pos = {
     1: (0, 0),  # position of node 1
     2: (1, 0),  # position of node 2
-    3: (2, 1),  # position of node 3
-    4: (2, -1),  # position of node 4
+    3: (1, 1),  # position of node 3
+    4: (1, -1),  # position of node 4
     8: (3, 2),
     5: (3, 1),
     6: (3, 0),
@@ -79,7 +64,7 @@ pos = {
     }
 
 start_node = 1
-end_node = 13
+end_node = 11
 
 G = nx.Graph()
 G.add_edges_from(edges)
