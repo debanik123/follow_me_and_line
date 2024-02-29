ae86d5bb8a9612d9ce68e0d624b4ab6703b3d483
Dj <debanikroy92@gmail.com>
2024-02-29 10:32:06 +0900

    1 to 9

--- line_following_DL/main.py
+++ line_following_DL/main.py
@@ -2,7 +2,9 @@ from graph_motion_planning import GraphAnalyzer
 
 if __name__ == "__main__":
     # Create an object of the GraphAnalyzer class
-    graph_analyzer = GraphAnalyzer('config/vtx_edg.json', 2,8)
+    start_node = 1
+    end_node = 9
+    graph_analyzer = GraphAnalyzer('config/vtx_edg.json', start_node, end_node)
 
     # Use the methods of the object
     path = graph_analyzer.find_shortest_path()
@@ -19,10 +21,16 @@ if __name__ == "__main__":
         print("Concatenated Moves:", concatenated_moves)
 
         if junction_nodes:
-            vertex_edge_dict = graph_analyzer.create_vertex_edge_dict(path, moves, junction_nodes)
-            print("Vertex Edge Dict:", vertex_edge_dict)
-
-            graph_analyzer.draw_graph(highlight_path=path, vertex_edge_dict=vertex_edge_dict)
+            if end_node not in junction_nodes:
+                vertex_edge_dict = graph_analyzer.create_vertex_edge_dict(path, moves, junction_nodes)
+                print("Vertex Edge Dict:", vertex_edge_dict)
+                graph_analyzer.draw_graph(highlight_path=path, vertex_edge_dict=vertex_edge_dict)
+            else:
+                print("000, Hi, I am executed")
+                graph_analyzer.draw_graph(highlight_path=path, vertex_edge_dict=None)
+        else:
+            print("111, Hi, I am executed")
+            graph_analyzer.draw_graph(highlight_path=path, vertex_edge_dict=None)
 
     else:
         print(f"No path found from {graph_analyzer.start_node} to {graph_analyzer.end_node}")
